2e36471ed894d0e6ed1ab2f4da2678c75fa01fd9
Florian Ziegler <florian@haptiq.studio>
2025-12-01 18:25:51 +0100

    Add one more approval fields example
    - This time only for e-commerce collections (when "selling images" is active)

--- filters/picu-approval-fields.php
+++ filters/picu-approval-fields.php
@@ -32,3 +32,35 @@ add_filter( 'picu_approval_fields', function( $fields, $post_id, $ident) {
 
 	return $fields;
 }, 10, 3 );
+
+
+/**
+ * When selling images, only show a comment box during checkout.
+ *
+ * @param array $fields The approval form fields.
+ * @param int $collection_id The collection post ID.
+ * @param string $ident The identifier for the client.
+ *
+ * @return array The adjusted approval form fields.
+ */
+add_filter( 'picu_approval_fields', function( $fields, $collection_id, $ident ) {
+	$ecommerce_active = get_post_meta( $collection_id, '_picu_collection_ecommerce', true );
+
+	if ( $ecommerce_active ) {
+		$fields = [
+			[
+				'type' => 'textarea',
+				'name' => 'comment',
+				'label' => 'Anything else you want us to know?',
+				'placeholder' => 'Leave a comment…',
+				'width' => 100,
+				'required' => false,
+				'value' => '',
+				'options' => '',
+				'validation' => '',
+			]
+		];
+	}
+
+	return $fields;
+}, 11, 3 );
